1f8f346d474dcb6fd6b20d1da6fc99e6d7f90c30
Alexander Neupokoev <alexsatter@gmail.com>
2026-05-20 12:10:42 +0300

    mmux 0.1.10

--- mmux.rb
+++ mmux.rb
@@ -4,12 +4,12 @@
 class Mmux < Formula
   desc "Keyboard-first TUI for Mattermost-compatible chat"
   homepage "https://github.com/iRootPro/mmux"
-  version "0.1.9"
+  version "0.1.10"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/iRootPro/mmux/releases/download/v0.1.9/mmux_v0.1.9_darwin_amd64.tar.gz"
-      sha256 "9d2b25a6b8ee3a6131c2cfd69c886f83b070dc3f260d9180eeefbfde1d8324b2"
+      url "https://github.com/iRootPro/mmux/releases/download/v0.1.10/mmux_v0.1.10_darwin_amd64.tar.gz"
+      sha256 "ed36a9113e32f1b6d4eb33b63c3ea42cc171aaec1a184d3b9d5db0c87ea138c8"
 
       define_method(:install) do
         install_binary
@@ -17,8 +17,8 @@ class Mmux < Formula
     end
 
     if Hardware::CPU.arm?
-      url "https://github.com/iRootPro/mmux/releases/download/v0.1.9/mmux_v0.1.9_darwin_arm64.tar.gz"
-      sha256 "b96e049ae3122e528e51e3383aebbce4aa9c521018f7eaaeee1e3346717f8c39"
+      url "https://github.com/iRootPro/mmux/releases/download/v0.1.10/mmux_v0.1.10_darwin_arm64.tar.gz"
+      sha256 "42cddd6837495d3d4ff402c84a1909e736dcde85a8e26a11b70168fce7bda0ba"
 
       define_method(:install) do
         install_binary
@@ -28,8 +28,8 @@ class Mmux < Formula
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/iRootPro/mmux/releases/download/v0.1.9/mmux_v0.1.9_linux_amd64.tar.gz"
-      sha256 "116be404029978b5e8bc54800c2f539c7b28e5d8cec2e50904c4db6a381dfb0a"
+      url "https://github.com/iRootPro/mmux/releases/download/v0.1.10/mmux_v0.1.10_linux_amd64.tar.gz"
+      sha256 "a61b008368eaaaf54be182d2a401ffaf9ab11d4ad3fc12265936998e026777bf"
 
       define_method(:install) do
         install_binary
@@ -37,8 +37,8 @@ class Mmux < Formula
     end
 
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/iRootPro/mmux/releases/download/v0.1.9/mmux_v0.1.9_linux_arm64.tar.gz"
-      sha256 "439b0a0d4818b62731705bce8edb7943c572184ddb51db45c8ce8af90fa6c457"
+      url "https://github.com/iRootPro/mmux/releases/download/v0.1.10/mmux_v0.1.10_linux_arm64.tar.gz"
+      sha256 "1e50f6adcbc66f8678cceddbd24503956cbc3fdab7e9fecb71e310036b063d29"
 
       define_method(:install) do
         install_binary
